121133b40134a01144aba159f9f169b78357560a
unknown <gythacinta88@gmail.com>
2024-10-11 20:47:12 +0700

    Pengumpulan Tugas pertemuan 1

---
+++ latihan1/index.php
@@ -0,0 +1,33 @@
+<?php
+echo "Selamat Datang";
+?>
+
+<?php
+$nama = "Siswan Syahputra";
+$umur = 33;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contoh Variabel PHP </title>
+</head>
+<body>
+<h1>jumlah, <?php echo $jumlah;?>!</h1>
+
+<p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+    
+</body>
+</html>
+
+<?php
+$a = 20;
+$b = 2;
+
+$jumlah = $a + $b;
+$kurang = $a - $b;
+$kali = $a * $b;
+$bagi = $a / $b;
+$sisa = $a % $b;
+?>
